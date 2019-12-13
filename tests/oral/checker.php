8a692b8d2194e07840f901e5dbedc9a9fd17dc39
ALearningCurve <21wwalling-sotolongo@psharvard.org>
2019-12-13 16:06:43 -0500

    Now it ranks by disease chance on formsss

--- tests/oral/checker.php
+++ tests/oral/checker.php
@@ -1,37 +1,32 @@
 <?php
 
   $diagnosis = [
-    "d1" => 0,
-    "d2" => 0,
+    "lmao-iosis" => 0,
+    "Terminal AaaAAaAAAAaAaA" => 0,
   ];
 
 
 
   // Get the first question's answer and do something
   if ($_POST["q1"] == "y") {
-    $diagnosis["d1"] += 1;
+    $diagnosis["lmao-iosis"] += 1;
   }
 
+  // Get the second question;s answer and do something
+  if ($_POST["q2"] == "y") {
+    $diagnosis["Terminal AaaAAaAAAAaAaA"] += 1;
+  }
+
+
+  // Sort the arrrrrrrr by the values in the arr in descending order
+  arsort($diagnosis);
 
   // Output all of the diagnosis and the value in dict
-  foreach (array_keys($diagnosis) as $key){
-    echo "<br>" . $key . ": " . $diagnosis[$key];
-  }
-
   // find the most likely diagnosis based on highest # in dict
-  $highest_disease = null;
-  $highest_disease_number = 0;
+  echo "<ol>";
   foreach (array_keys($diagnosis) as $key){
-    $temp = $diagnosis[$key];
-
-    if ($temp > $highest_disease_number){
-
-      $highest_disease = $key;
-      $highest_disease_number = 0;
-    }
+    echo "<li>". $key ." with number weight ". $diagnosis[$key] ."</li>";
   }
-
-  echo "<br>Disease With Highest Chance: ". $highest_disease . " with number weight " . $highest_disease_number;
-
+  echo "</ol>";
 
 ?>
