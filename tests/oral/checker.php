4bf5ad60d568a2a342cc9b5b82d3791f485a59cb
ALearningCurve <21wwalling-sotolongo@psharvard.org>
2019-12-13 15:17:26 -0500

    It knows everything
    Now it weighs based on question dict vals to determine what you have

--- tests/oral/checker.php
+++ tests/oral/checker.php
@@ -1,4 +1,37 @@
 <?php
-  echo "<br>Question 1:: " . $_POST["q1"];
-  echo "<br>Question 2:: " . $_POST["q2"];
+
+  $diagnosis = [
+    "d1" => 0,
+    "d2" => 0,
+  ];
+
+
+
+  // Get the first question's answer and do something
+  if ($_POST["q1"] == "y") {
+    $diagnosis["d1"] += 1;
+  }
+
+
+  // Output all of the diagnosis and the value in dict
+  foreach (array_keys($diagnosis) as $key){
+    echo "<br>" . $key . ": " . $diagnosis[$key];
+  }
+
+  // find the most likely diagnosis based on highest # in dict
+  $highest_disease = null;
+  $highest_disease_number = 0;
+  foreach (array_keys($diagnosis) as $key){
+    $temp = $diagnosis[$key];
+
+    if ($temp > $highest_disease_number){
+
+      $highest_disease = $key;
+      $highest_disease_number = 0;
+    }
+  }
+
+  echo "<br>Disease With Highest Chance: ". $highest_disease . " with number weight " . $highest_disease_number;
+
+
 ?>
