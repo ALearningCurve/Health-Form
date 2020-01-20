4c602828e3b9ada419c792081528e7dd35314c02
Tjiang1688 <31900575+Tjiang1688@users.noreply.github.com>
2020-01-20 09:40:43 -0500

    eek

--- tests/oral/checker.php
+++ tests/oral/checker.php
@@ -1,15 +1,14 @@
 <?php
 
   $diagnosis = [
-    "lmao-iosis" => 0,
+    "Common Cold" => 0,
     "Terminal AaaAAaAAAAaAaA" => 0,
   ];
 
 
-
   // Get the first question's answer and do something
   if ($_POST["q1"] == "y") {
-    $diagnosis["lmao-iosis"] += 1;
+    $diagnosis["Common Cold"] += 1;
   }
 
   // Get the second question;s answer and do something
@@ -18,14 +17,19 @@
   }
 
 
+
   // Sort the arrrrrrrr by the values in the arr in descending order
   arsort($diagnosis);
+
 
   // Output all of the diagnosis and the value in dict
   // find the most likely diagnosis based on highest # in dict
   echo "<ol>";
   foreach (array_keys($diagnosis) as $key){
-    echo "<li>". $key ." with number weight ". $diagnosis[$key] ."</li>";
+    // only shows the ones with likelihood more than 0
+    if ($diagnosis[$key] >0){
+        echo "<li>". $key ." with number weight ". $diagnosis[$key] ."</li>";
+    }
   }
   echo "</ol>";
 
