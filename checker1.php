aadabd825405287e25919cb4357628df33762af7
Tjiang1688 <31900575+Tjiang1688@users.noreply.github.com>
2020-02-03 13:00:43 -0500

    u ok will

--- checker1.php
+++ checker1.php
@@ -12,26 +12,175 @@
   // AAAAAAAAAAAAAAAAAAAA substitute for undetermined thing that i will research later
 
 
-  // if person coughing
-  if ($_POST["cough"] == "y") {
-    $diagnosis["Common Cold"] += 1;
-    $diagnosis["Pneumonia"] += 1;
+  // ears
+  if ($_POST["earache"] == "y") {
+    $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
   }
 
-  // if person itchy throat
-  if ($_POST["itchy throat"] == "y") {
-    $diagnosis["Fever"] += 1;
-  }
-
-    // if person dry mouth
-    if ($_POST["dry mouth"] == "y") {
+  if ($_POST["hearingloss"] == "y") {
       $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
     }
 
-// if person tight jaw/can't chew
-  if ($_POST["tight jaw"] == "y") {
-      $diagnosis["Common Cold"] += 1;
+  if ($_POST["eardischarge"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
     }
+
+  if ($_POST["clog"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["itchyear"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["ringing"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+
+  //mouth
+  if ($_POST["cough"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["itchythroat"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["drymouth"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["toothache"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["bumpytongue"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["coughingblood"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["vomit"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["gumpain"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+
+  //eyes
+  if ($_POST["eyepain"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["redeyes"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["itchyeyes"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["wateryeyes"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["visionloss"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["twitch"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["droop"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["eyedischarge"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["doublevision"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+
+  //head
+  if ($_POST["memory"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["dizzy"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["hair"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["bumps"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["headache"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["numb"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["fever"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+
+  //nose
+  if ($_POST["breathing"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["misshapen"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["runny"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["stuffy"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["bruising"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["swelling"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["bloodynose"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["nosedischarge"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["itchynose"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
+  if ($_POST["nosepain"] == "y") {
+      $diagnosis["AAAAAAAAAAAAAAAAAAAA"] += 1;
+    }
+
 
 
 
